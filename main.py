e51ed45f9842940821b258c9ab19baafc7920a01
Mariann Mesa <mesa.mariann8@gmail.com>
2026-01-21 10:25:44 +0000

    added while loop

--- main.py
+++ main.py
@@ -11,3 +11,40 @@ def divide(a, b):
     if b == 0:
         return "Enter: Division by zero is not allowed."
         return a/b
+
+while True:
+    print("\n=== Persistent Python Calculator ===")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Choose an operation (1-5): ")
+
+    if choice == "5":
+        print("Calculator terminated.")
+        break
+
+    if choice not in ["1", "2", "3", "4"]:
+        print("Invalid choice. Try again.")
+        continue
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
+        continue
+
+    if choice == "1":
+        result = add(num1, num2)
+    elif choice == "2":
+        result = subtract(num1, num2)
+    elif choice == "3":
+        result = multiply(num1, num2)
+    elif choice == "4":
+        result = divide(num1, num2)
+
+    print("Result:", result)
+  
