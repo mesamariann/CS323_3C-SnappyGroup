53e5c93bd484db23cb261947f87453b462d1388e
galendiz <rfsgalendez@gmail.com>
2026-01-21 10:03:42 +0000

    fnjdsbfkjsbjkf

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-#Franco_Galendez
-def add (a, b):
-    return a+b
-
-#Aldwyn_Betonio
-def subtract(a, b):
-    return a-b
-
-#Gwynette_Galleros
-def multiply(a, b):
-    return a*b
+def add(a, b):
+    return a + fjjfjfjfnnfnfnfhsydgfsfysuy
