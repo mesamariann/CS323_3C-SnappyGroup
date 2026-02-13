088b51b6a2c199900d48a42c2d66ff7879a8e99d
Aldwyn Betonio <betonio.aldwyn@gmail.com>
2026-02-13 21:11:52 +0800

    added deduction functions

---
+++ Lab_activity_3/main.py
@@ -0,0 +1,14 @@
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
+# Task deduction functions
+def compute_sss(salary):
+    return salary * 0.045
+
+def compute_philhealth(salary):
+    return salary * 0.025
+
+def compute_pagibig(salary):
+    return salary * 0.02
+
+def compute_tax(salary):
+    return salary * 0.10
