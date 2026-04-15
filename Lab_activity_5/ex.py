8a94f9f8f5a9c57f4de1c2c86f5876c7b5380380
franco <rfsgalendez@gmail.com>
2026-04-15 23:08:10 +0800

    Rename merge_sort to sequential_sort and adjust logic
    Refactor merge_sort to sequential_sort and update conditions.

--- Lab_activity_5/ex.py
+++ Lab_activity_5/ex.py
@@ -1,40 +1,36 @@
 import random
+import time
+from multiprocessing import Process, Queue
+
 
 def generate_data(n):
     data = [random.randint(1, 1000000) for _ in range(n)]
     return data
 
-if __name__ == "__main__":
-    print("Parallel vs Sequential Algorithms")
-
+# Sequential Merge Sort
 def merge(left, right):
-    
     result = []
     i = j = 0
-    
+
     while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
+        if left[i] < right[j]:
             result.append(left[i])
             i += 1
         else:
             result.append(right[j])
             j += 1
-    
+
     result.extend(left[i:])
     result.extend(right[j:])
     return result
 
-def merge_sort(data):
-    
+
+def sequential_sort(data):
     if len(data) <= 1:
         return data
-    
+
     mid = len(data) // 2
-    left = merge_sort(data[:mid])
-    right = merge_sort(data[mid:])
+    left = sequential_sort(data[:mid])
+    right = sequential_sort(data[mid:])
+
     return merge(left, right)
-
-
-
-
-
